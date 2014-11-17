6d80d88ab9f5e698f1794a6f1c79f2769bbc0451
wso277 <oliveira.wilson93@gmail.com>
2014-11-17 16:19:44 +0000

    Extracted some variables

--- src/mainPackage/Main.java
+++ src/mainPackage/Main.java
@@ -6,13 +6,14 @@ import uchicago.src.sim.engine.SimInit;
 public class Main {
 
     private static final boolean BATCH_MODE = true;
+    private static final int MAX_NUMBER_AGENTS = 30;
     private static SocialGroupModel socialModel;
 
     public static void main(String[] args) {
         boolean runMode = !BATCH_MODE;
 
         SimInit init = new SimInit();
-        socialModel = new SocialGroupModel("Social Group Model", 20);
+        socialModel = new SocialGroupModel("Social Group Model", MAX_NUMBER_AGENTS);
         init.loadModel(socialModel, null, runMode);
     }
 
