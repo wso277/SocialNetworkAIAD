655b7a658d39289382ffddac5a62aca13967b42e
jmmesquitacardoso <jmmesquitacardoso@gmail.com>
2014-11-26 10:54:20 +0000

    faz push meu, decaminho ficas sem net e ficamos aqui a mamar no dedo

--- src/util/FIRE.java
+++ src/util/FIRE.java
@@ -1,8 +1,11 @@
 package util;
 
+import java.util.ArrayList;
+
 public class FIRE {
 
     private static FIRE instance = null;
+    private final int m = 200;
 
     private float omega;
 
@@ -24,7 +27,38 @@ public class FIRE {
         return instance;
     }
 
-    private float calculateTI() {
+    private float calculateTI(ArrayList<Float> omegas, ArrayList<Float> values) {
+        float result = 0f;
+        if (omegas.size() != values.size()) {
+            return -1;
+        }
+        for (int i = 0; i < omegas.size(); i++) {
+            result += omegas.get(i)*values.get(i);
+        }
+        return result;
+    }
 
+    private float calculatePN(int n) {
+        float result;
+        if (n <= m) {
+            result = (float)n/m;
+        } else {
+            result = 1;
+        }
+        return result;
+    }
+
+    private float calculatePD(ArrayList<Float> omegas, ArrayList<Float> values, float ti) {
+        float result = 0, res = 0;
+        for (int i = 0; i < omegas.size(); i++) {
+            res += (omegas.get(i)*Math.abs(values.get(i)-ti))/2;
+        }
+        result = 1 - res;
+        return result;
+    }
+
+    private float calculatePTI(float pn, float pd) {
+        float pti = pn*pd;
+        return pti;
     }
 }
